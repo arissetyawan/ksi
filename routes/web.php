0e2a5c2df887df67821750d74206117b7322cc72
Arissan <arissetyawan.email@gmail.com>
2016-09-22 18:32:12 +0700

    r1

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/downloads', function () {
+    return view('downloads');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
